cfe968e06a0752464a216e315bd1ab7bfcb5463d
Konstantin Bondar <konstantin.bondar@postindustria.com>
2019-06-26 11:23:34 +0300

    Initial Commit

---
+++ DoomPrequel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoomPrequel
+//
+//  Created by Konstantin Bondar on 26.06.19.
+//  Copyright © 2019 SocialTermite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
